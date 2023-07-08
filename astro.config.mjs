729c80c47c4151c1ecbcf1a70cca6ba48ff973b0
R. Ilham <radenpioneer@users.noreply.github.com>
2023-07-08 13:26:37 +0700

    add basic layout and custom typefaces

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: ['node_modules'],
+        },
+      },
+    },
+  },
+})
