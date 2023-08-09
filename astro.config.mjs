839826f96e581c8ac6708b8afa759673b10268f3
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-08-09 15:35:03 +0000

    use built-in astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import sitemap from '@astrojs/sitemap'
 import pwa from '@vite-pwa/astro'
-import compress from 'astro-compress'
 
 // Vite Plugins
 import Icons from 'unplugin-icons/vite'
@@ -17,6 +16,7 @@ import manifest from './src/components/scripts/manifest.json' assert { type: 'js
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dev.radenpioneer.blog',
+  compressHTML: true,
   integrations: [
     react(),
     markdoc(),
@@ -27,16 +27,6 @@ export default defineConfig({
       srcDir: 'src/components/scripts',
       filename: 'sw.ts',
       manifest,
-    }),
-    compress({
-      html: {
-        collapseWhitespace: true,
-        collapseInlineTagWhitespace: true,
-        conservativeCollapse: true,
-        removeRedundantAttributes: true,
-        sortAttributes: true,
-        sortClassName: true,
-      },
     }),
   ],
   redirects: {
