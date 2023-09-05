ce940901ca2b042861465fdcda55af1f89fe3e17
R. Ilham <radenpioneer@users.noreply.github.com>
2023-09-05 15:23:31 +0700

    set up styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dev.radenpioneer.blog',
-  compressHTML: true,
+  compressHTML: process.env.NODE_ENV === 'production',
   integrations: [],
 })
