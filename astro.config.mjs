d0c596ccccd9f02abe0e84bd09cc69834e7022f2
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-09-14 09:04:57 +0000

    move umami script to worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import vercel from '@astrojs/vercel/serverless'
-
 import sitemap from '@astrojs/sitemap'
+
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dev.radenpioneer.blog',
   compressHTML: process.env.NODE_ENV === 'production',
-  integrations: [react(), markdoc(), sitemap()],
+  integrations: [react(), markdoc(), sitemap(), partytown()],
   output: 'hybrid',
   adapter: vercel(),
 })
