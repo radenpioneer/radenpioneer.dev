54f6f6023729b2818b0275b0c2af25bcc25398c9
R. Ilham Sastronegoro <radenpioneer@hotmail.com>
2023-12-08 04:27:42 +0000

    replace with pictureelement

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,16 @@
-import type { FC } from 'react'
-import type { GetImageResult } from 'astro'
+import type { FC, PropsWithChildren } from 'react'
 import typestyle from '~/styles/fonts.module.scss'
 import type { SiteSettings } from '~/data/site'
 
-export const Header: FC<{
-  site: SiteSettings['publication']
-  profileImage: GetImageResult
-}> = ({ site, profileImage }) => {
+export const Header: FC<
+  PropsWithChildren<{
+    site: SiteSettings['publication']
+  }>
+> = ({ site, children }) => {
   return (
     <nav className="flex flex-col items-center py-8 gap-4">
       <div className="flex flex-col items-center gap-1">
-        <a href="/">
-          <img
-            src={profileImage.src}
-            {...profileImage.attributes}
-            className="w-[50px] h-[50px] object-cover rounded-full shadow-md"
-            alt={site.title}
-            loading="eager"
-          />
-        </a>
+        <a href="/">{children}</a>
         <a
           className={`${typestyle.__heading} font-extrabold text-xl text-center`}
           href="/"
