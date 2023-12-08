051422722666a3d88d15b89acc64b000633eeff5
R. Ilham Sastronegoro <radenpioneer@hotmail.com>
2023-12-08 15:55:28 +0000

    reorganize components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import vercel from '@astrojs/vercel/serverless'
 import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://radenpioneer-dev.vercel.app',
   compressHTML: process.env.NODE_ENV === 'production',
-  integrations: [react(), markdoc(), sitemap(), partytown()],
+  integrations: [react(), markdoc(), sitemap()],
   output: 'hybrid',
   adapter: vercel({
     imageService: true,
+    // imagesConfig: {
+    //   sizes: [35, 50, 80, 120, 160, 320, 480, 640, 1280],
+    // },
   }),
   image: {
     domains: ['cdn.hashnode.com'],
